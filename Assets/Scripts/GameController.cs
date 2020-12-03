c4012fefca465e0d27a081670b63b9084975e236
ss5432g <sugimoto@phys2.med.osaka-u.ac.jp>
2020-12-03 21:22:08 +0900

    hello

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ClearUI;
+    public GameObject GameOverUI;
+
+    public void Update()
+    {
+        int count = GameObject.FindGameObjectsWithTag("Block").Length;
+        int count1 = GameObject.FindGameObjectsWithTag("Player").Length;
+
+        if (count == 0)
+        {
+            // ClearUIをアクティブにする
+            ClearUI.SetActive(true);
+        }
+        if (count1 == 0)
+        {
+            //GameOverUIをアクティブにする
+            //GameOverUI.SetActive(true);
+
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
